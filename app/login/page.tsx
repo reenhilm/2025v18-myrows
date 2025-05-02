36fbf3b921f6109896b5af42e1a0c2cae6cdc371
Christian Rönnholm <christian@kajal.se>
2025-05-02 13:57:35 +0200

    styled loginpage

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,12 @@
+import { Button } from '@/components/ui/button'
 import { login } from './actions'
 export default function LoginPage() {
     return (
         <main className="col-start-2 -col-end-2 flex flex-col items-center justify-center">
-            <form>
-                <label htmlFor="email">Email:</label>
-                <input id="email" name="email" type="email" required />
-                <label htmlFor="password">Password:</label>
-                <input id="password" name="password" type="password" required />
-                <button formAction={login}>Log in</button>
+            <form className="flex flex-col md:flex-row gap-5">
+                <input id="email" name="email" type="email" required placeholder="email@email.com" aria-label="email" />
+                <input id="password" name="password" type="password" required aria-label="password" />
+                <Button className='align-center' formAction={login}>Log in</Button>
             </form>
         </main>
     )
