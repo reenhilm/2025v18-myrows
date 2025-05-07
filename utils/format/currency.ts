584c0d083d7aa3425646caede3e0916145cdc943
Christian Rönnholm <christian@kajal.se>
2025-05-07 15:28:16 +0200

    removed unused files

--- utils/format/currency.ts
+++
@@ -1,9 +0,0 @@
-export function formatSEK(decimale: number): string {
-    const formatted = new Intl.NumberFormat('sv-SE', {
-        style: 'currency',
-        currency: 'SEK',
-        currencyDisplay: 'narrowSymbol',
-        minimumFractionDigits: 2,
-    }).format(decimale);
-    return formatted;
-}
