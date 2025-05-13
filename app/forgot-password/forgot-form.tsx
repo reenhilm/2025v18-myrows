4e1b88f1f60b2e1eb416f057a56871bceee0f64b
Christian Rönnholm <christian@kajal.se>
2025-05-13 15:07:13 +0200

    feat: refactor forgot password functionality into separate ForgotForm component (easier to read for friend)

---
+++ app/forgot-password/forgot-form.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { toast } from "sonner"
+import { forgotPassword } from '@/utils/api-client';
+import { isApiError } from '@/utils/type-guards';
+import { successForgotPasswordSent } from '@/constants';
+
+
+export default function ForgotForm() {
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const email = formData.get("email")?.toString() || "";
+        const result = await forgotPassword(email);
+
+        if (isApiError(result)) {
+            toast.error("Error", { description: result.message });
+        }
+        else {
+            toast.success("Success", { description: successForgotPasswordSent });
+        }
+    }
+
+    return (
+        <form className="flex flex-col md:flex-row gap-5" onSubmit={handleSubmit} >
+            <Input id="email" name="email" type="email" required placeholder="email@email.com" aria-label="email" />
+            <Button className='align-center' type="submit">Send</Button>
+        </form >
+    )
+}
--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -1,36 +1,11 @@
-'use client';
-import { useRef } from 'react';
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { toast } from "sonner"
-import { forgotPassword } from '@/utils/api-client';
-import { isApiError } from '@/utils/type-guards';
-import { successForgotPasswordSent } from '@/constants';
+import ForgotForm from "./forgot-form";
 
-export default function ForgotPassword() {
-    const formRef = useRef<HTMLFormElement>(null);
-
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const formData = new FormData(formRef.current!);
-        const email = formData.get("email")?.toString() || "";
-        const result = await forgotPassword(email);
-
-        if (isApiError(result)) {
-            toast.error("Error", { description: result.message });
-        }
-        else {
-            toast.success("Success", { description: successForgotPasswordSent });
-        }
-    }
+export default function ForgotPassword() {    
 
     return (
         <main className="col-start-2 -col-end-2 flex flex-col items-center justify-center">
             <p>Skicka glömt lösenordsidan</p>
-            <form className="flex flex-col md:flex-row gap-5" ref={formRef} onSubmit={handleSubmit}>
-                <Input id="email" name="email" type="email" required placeholder="email@email.com" aria-label="email" />
-                <Button className='align-center' type="submit">Send</Button>
-            </form>
+            <ForgotForm />
         </main>
     )
 }
